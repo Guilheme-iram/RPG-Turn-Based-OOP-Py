8b5124ce2b9fd1e80ddb44e4c088b6c5b0081a48
Guilherme Iram <guilherme.iram@gmail.com>
2022-05-11 11:45:47 -0300

    Early version of the game - simple

--- RPG-Turn-Based.py
+++ RPG-Turn-Based.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May 11 09:42:59 2022
 
+"""
 @author: Guilherme
 """
 
@@ -17,11 +16,20 @@ class Creature():
               f"Mana: {self.mp:^4}")
         
     def lose_hp(self, hp_lost):
-        self.hp -= hp_lost
+        if self.hp > hp_lost:
+            self.hp -= hp_lost
+        else:
+            self.hp = 0
     
     def lose_mp(self, mp_lost):
-        self.mp -= mp_lost
-
+        if self.mp > mp_lost:    
+            self.mp -= mp_lost
+        else:
+            self.mp = 0
+    
+    def is_alive(self):
+        return self.hp > 0
+    
 class Hero(Creature):
     def __init__(self, hp, mp):
         super().__init__(hp, mp)
@@ -30,11 +38,33 @@ class Hero(Creature):
         print("Player")
         super().print_status()
         print()
-    
+        
+    def action(self, target):
+        
+        act = input("""What you gonna do?
+        A - Attack | H - Potion | S - Status of Battle | P - pass turn
+        """)
+        if act in "aA":
+            self.attack(target=target)
+        elif act in "hH":
+            self.__use_potion()
+        elif act in "sS":
+            self.status()
+            target.status()
+        elif act in "pP":
+            print("You've pass your turn")
+        else:
+            pass
+        
     def attack(self, target):
-        hit = 5
+        hit = 6
         print(f"Hero attack | {hit} of damage\n")
         target.lose_hp(hit)
+    
+    def __use_potion(self):
+        cure = 2
+        self.hp += cure 
+        print(f"Hero heal | {cure} of hp\n")
         
 class Enemy(Creature):
     def __init__(self, hp, mp):
@@ -56,16 +86,13 @@ def main():
     warrior = Hero(100, 20)
     wolf = Enemy(40, 10)
     
-    warrior.status()
-    wolf.status()
-    
-    warrior.attack(wolf)
-    wolf.attack(warrior)
-    warrior.attack(wolf)
-    wolf.attack(warrior)
+    while warrior.is_alive() and wolf.is_alive():
+        warrior.action(wolf)
+        wolf.attack(warrior)
     
     warrior.status()
     wolf.status()
+    print("Player Wins!") if warrior.is_alive() else print("Player Losed!")
     
     
 if __name__ == '__main__':
